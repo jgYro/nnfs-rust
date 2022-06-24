0f951e50960b4e6f4e44e62093846664d8ad7fdb
Jericho C. Gregory <yro@Jerichos-MacBook-Air.local>
2022-06-24 09:59:15 -0400

    Both examples for hard coding neurons through pg.29

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
 fn main() {
-    println!("Hello, world!");
+    // Pg. 26
+    // println!(
+    //     "This is the first example output: {:?}",
+    //     hard_coded_neuron_ex1()
+    // );
+
+    // Pg. 29
+    // println!(
+    //     "This is the second example output: {:?}",
+    //     hard_coded_neuron_ex2()
+    // )
 }
+
+// Pg.26 Hard coding a neuron with 3 inputs and 3 weights
+// fn hard_coded_neuron_ex1() -> f32 {
+//     let inputs = vec![1.0, 2.0, 3.0];
+//     let weights = vec![0.2, 0.8, -0.5];
+//     let bias = 2.0;
+
+//     let output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias;
+
+//     return output;
+// }
+
+// Pg.29 Hard coding a neuron with 4 inputs and 4 weights
+// fn hard_coded_neuron_ex2() -> f32 {
+//     let inputs = vec![1.0, 2.0, 3.0, 2.5];
+//     let weights = vec![0.2, 0.8, -0.5, 1.0];
+//     let bias = 2.0;
+
+//     let output = inputs[0] * weights[0]
+//         + inputs[1] * weights[1]
+//         + inputs[2] * weights[2]
+//         + inputs[3] * weights[3]
+//         + bias;
+
+//     return output;
+// }
